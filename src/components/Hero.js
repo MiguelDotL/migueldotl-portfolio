24c97f0545afe04a44423a91d872238acd0726b7
MiguelDotL <miguel.d.lozano@gmail.com>
2022-10-22 11:03:53 -0400

    build and style hero section component, add logic for typing animation

---
+++ src/components/Hero.js
@@ -0,0 +1,99 @@
+import "../assets/styles/Hero.css";
+import bitmojiSpaceMan from "../assets/images/bitmoji/bitmoji-spaceman-2.png";
+import bitmojiSpacePlanet from "../assets/images/bitmoji/bitmoji-space-planet-2.png";
+import { Col, Container, Row } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import { useEffect, useState } from "react";
+
+const Hero = () => {
+    // const [isDeleting, setIsDeleting] = useState(false);
+    const [isTyping, setIsTyping] = useState(true);
+    const [jobTitle, setJobTitle] = useState("");
+    const [roleCount, setRoleCount] = useState(0);
+    const [typingDelay, setTypingDelay] = useState(200 - Math.random() * 50);
+    // const [typingDelay, setTypingDelay] = useState(100);
+    const roles = ["Front-End", "Back-End", "Full-Stack Developer   "];
+    const pauseTime = 3456; // time between typing and deleting
+
+    useEffect(() => {
+        let typingTicker = setInterval(() => {
+            doTyping();
+        }, typingDelay);
+
+        return () => {
+            clearInterval(typingTicker);
+        };
+    }, [jobTitle]);
+
+    const doTyping = () => {
+        let currentRole = roleCount % roles.length;
+        let fullText = roles[currentRole];
+        let currentText = isTyping
+            ? fullText.substring(0, jobTitle.length + 1)
+            : fullText.substring(0, jobTitle.length - 1);
+
+        setJobTitle(currentText);
+
+        if (!isTyping) {
+            // setTypingDelay((prevTypingDelay) => {
+            //     console.log("typingDelay: " + prevTypingDelay);
+            //     console.log("typingDelay / 2: " + prevTypingDelay / 2);
+            //     console.log("currentText: ", currentText);
+
+            //     return prevTypingDelay / 2;
+            // });
+            setTypingDelay(100);
+        }
+
+        if (isTyping && currentText === fullText) {
+            setIsTyping(false);
+            setTypingDelay(pauseTime);
+        } else if (!isTyping && currentText === "") {
+            setIsTyping(true);
+            setRoleCount(roleCount + 1); // roleCount++?
+            setTypingDelay(321);
+        }
+    };
+
+    return (
+        <section id="about-me" className="hero about-me">
+            <div className="overlay">
+                <Container>
+                    <Row className="align-items-center">
+                        <Col xs={12} md={7} xl={7}>
+                            <span className="tagline">Thanks for dropping by</span>
+                            <h1 className="intro-header">Hi, I'm Miguel!</h1>
+                            <h1>
+                                I'm a <span className="typing-text">{jobTitle}</span>
+                            </h1>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Facere quae dolore dolores excepturi praesentium
+                                doloremque est ducimus ad in, sunt reprehenderit ut optio
+                                ipsum, cumque numquam, ea facilis? Expedita, et.
+                            </p>
+                            <button
+                                className="hero-contact-button"
+                                onClick={() =>
+                                    console.log("hello from the hero contact button!")
+                                }
+                            >
+                                Let's Chat
+                                <ArrowRightCircle size={25} />
+                            </button>
+                        </Col>
+                        <Col className="image-col" xs={12} md={5} xl={5}>
+                            <img
+                                className="floating-image"
+                                src={bitmojiSpacePlanet}
+                                alt="Floating Caricature"
+                            />
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
