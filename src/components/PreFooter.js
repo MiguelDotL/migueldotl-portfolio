babf3b0514f29d719c548f0c15cdf858db000a57
MiguelDotL <miguel.d.lozano@gmail.com>
2022-10-24 13:57:01 -0400

    add built with section

--- src/components/PreFooter.js
+++ src/components/PreFooter.js
@@ -1,18 +1,27 @@
-import { Col, Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import reactIcon from "../assets/images/icons/react-original.svg";
+import bootstrapIcon from "../assets/images/icons/bootstrap-original.svg";
 
 const PreFooter = () => {
     return (
         <Col lg={12}>
-            <Row className="justify-content-center">
-                <div id="built-with" className="built-with">
-                    <h3>
-                        This Site Was Built With <span>♥️</span> Using:
-                    </h3>
-                    <span>React</span>
-                    <span>+</span>
-                    <span>Bootstrap</span>
+            <div id="built-with" className="built-with">
+                <h3>
+                    This Site Was <br />
+                    Built With <span>♥️</span> Using:
+                </h3>
+                <div className="tooling-icons">
+                    <div className="tooling-icon react-icon">
+                        <img src={reactIcon} alt="react-icon" />
+                        <span>React</span>
+                    </div>
+                    <span className="plus-sign">+</span>
+                    <div className="tooling-icon bootstrap-icon">
+                        <img src={bootstrapIcon} alt="bootstrap-icon" />
+                        <span>Bootstrap</span>
+                    </div>
                 </div>
-            </Row>
+            </div>
         </Col>
     );
 };
